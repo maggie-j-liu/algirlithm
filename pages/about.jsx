d6df05904c791c3760f00d442787e0f17dc2eb9c
maggie-j-liu <maggie.j.liu@gmail.com>
2021-10-05 22:08:02 -0700

    add tarushii and kelly

--- pages/about.jsx
+++ pages/about.jsx
@@ -4,17 +4,19 @@ import Organizer from "@/components/Organizer";
 import { Title, Subtitle } from "@/components/Titles";
 import Blobs from "@/components/Blobs";
 import amy from "@/public/amy.jpeg";
+import kelly from "@/public/kelly.jpeg";
 import maggie from "@/public/maggie.png";
 import melody from "@/public/melody.png";
 import nicole from "@/public/nicole.jpeg";
+import tarushii from "@/public/tarushii.jpeg";
 
 const organizers = [
   { name: "Amy Chang", role: "organizer", image: amy },
-  { name: "Kelly Zhou", role: "organizer" },
+  { name: "Kelly Zhou", role: "organizer", image: kelly },
   { name: "Maggie Liu", role: "organizer", image: maggie },
   { name: "Melody Yu", role: "organizer", image: melody },
   { name: "Nicole Xu", role: "organizer", image: nicole },
-  { name: "Tarushii Goel", role: "organizer" },
+  { name: "Tarushii Goel", role: "organizer", image: tarushii },
 ];
 
 const About = () => {
