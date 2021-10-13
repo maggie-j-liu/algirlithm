6a4deb7eecd410a7ad6d6eff7bfb41084732a9c9
cskitty <melody.yue.yu@gmail.com>
2021-10-12 18:57:55 -0700

    fixed

--- pages/logistics.jsx
+++ pages/logistics.jsx
@@ -35,18 +35,14 @@ const Logistics = () => {
         <Section>
           <Subtitle>Contest Format</Subtitle>
           <SubSubtitle>Divisions</SubSubtitle>
-          <p>
-            Each contest contains 8 algorithmic problems of varying difficulty.
-            <ul className="list-disc">
-              <li>
-                Novice (correlates to basic to USACO bronze to easy USACO
-                silver)
-              </li>
-              <li>
-                Advanced (correlates to easy USACO silver to regular gold)
-              </li>
-            </ul>
-          </p>
+             <p> Each contest contains 8 algorithmic problems of varying difficulty.</p>
+
+             <div className="prose prose-lg text-black-500 mx-auto lg:px-8">
+             <ul class="list-disc">
+                  <li> Novice (correlates to basic to USACO bronze to easy USACO silver)</li>
+                  <li>Advanced (correlates to easy USACO silver to regular gold) </li>
+                  </ul>
+              </div>
           <SubSubtitle>Available Languages</SubSubtitle>
           <p>Java, C++, Python</p>
           <SubSubtitle>Contest platform - Hackerrank</SubSubtitle>
