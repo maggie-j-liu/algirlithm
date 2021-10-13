d7d67645492d7aa8c8a1b4e510cdbf554cd352d4
cskitty <melody.yue.yu@gmail.com>
2021-10-12 18:42:13 -0700

    adjusted divisions

--- pages/logistics.jsx
+++ pages/logistics.jsx
@@ -36,9 +36,11 @@ const Logistics = () => {
           <Subtitle>Contest Format</Subtitle>
           <SubSubtitle>Divisions</SubSubtitle>
           <p>
-            Each contest contains 8 algorithmic problems of varying difficulty.
-            Novice (correlates to basic to USACO bronze to easy USACO silver)
-            Advanced (correlates to easy USACO silver to regular gold)
+            Each contest contains 8 algorithmic problems of varying difficulty.  
+<ul class="list-disc">
+  <li>Novice (correlates to basic to USACO bronze to easy USACO silver) </li>
+  <li>Advanced (correlates to easy USACO silver to regular gold)   </li>
+</ul>
           </p>
           <SubSubtitle>Available Languages</SubSubtitle>
           <p>Java, C++, Python</p>
