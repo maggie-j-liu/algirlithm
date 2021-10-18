b2ffc3fbe26f3c1126a7acceb5e94cf7766fd601
maggie-j-liu <maggie.j.liu@gmail.com>
2021-10-18 16:13:09 -0700

    change og image

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
         title="alGIRLithm Programming Contest"
         description="alGIRLithm is a programming competition for high school girls."
         url=""
-        image="/ogimage.png"
+        image="/algirlithm.png"
         twitterCard="summary_large_image"
       >
         <meta name="theme-color" content="#FB7185" />
